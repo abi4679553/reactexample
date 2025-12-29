93e068dc301c4e970fa4b08ae32c1eae5f9ff9fe
abi4679553 <abivengadajalam7708@gmail.com>
2025-12-29 10:39:12 +0530

    Table.js

--- src/components/Table.js
+++ src/components/Table.js
@@ -17,24 +17,16 @@ const Table = () => {
 
   return (
     <div>
-      <h2>Form to Table</h2>
+      <h1 className="text-2xl text-blue-700 m-3 text-center ">Hello fill  the form </h1>
+      <h2 className=" text-xl p-3 text-center ">Form to Table</h2>
+
 
       <form  onSubmit={handleSubmit}>
-        <input className=" border-2 border-solid"
-          type="text"
-          placeholder="Enter Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
+        <input className=" border-2 border-solid m-3 " type="text" placeholder="Enter Name" value={name}   onChange={(e) => setName(e.target.value)}/>
 
-        <input
-          type="number"
-          placeholder="Enter Age"
-          value={age}
-          onChange={(e) => setAge(e.target.value)}
-        />
+        <input  className="m-3 border-2 border-solid"  type="number" placeholder="Enter Age" value={age} onChange={(e) => setAge(e.target.value)} />
 
-        <button type="submit">Submit</button>
+        <button className="text-xl text-white bg- bg-pink-600 p-2 m-3 "  type="submit">Submit</button>
       </form>
 
       <table border="1">
