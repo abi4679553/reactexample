df8064c377c1f720a6a17ed4a62973798f98cb9a
abi4679553 <abivengadajalam7708@gmail.com>
2025-12-30 20:32:01 +0530

    Table.js

--- src/components/Table.js
+++ src/components/Table.js
@@ -21,19 +21,22 @@ const Table = () => {
       <h2 className=" text-xl p-3 text-center ">Form to Table</h2>
 
 
-      <form  onSubmit={handleSubmit}>
-        <input className=" border-2 border-solid m-3 " type="text" placeholder="Enter Name" value={name}   onChange={(e) => setName(e.target.value)}/>
+      <form onSubmit={handleSubmit}>
+        <input className=" border-2 border-solid m-3 " type="text" placeholder="Enter Name" value={name} onChange={(e) => setName(e.target.value)} />
 
-        <input  className="m-3 border-2 border-solid"  type="number" placeholder="Enter Age" value={age} onChange={(e) => setAge(e.target.value)} />
+        <input className="m-3 border-2 border-solid" type="number" placeholder="Enter Age" value={age} onChange={(e) => setAge(e.target.value)} />
 
-        <button className="text-xl text-white bg- bg-pink-600 p-2 m-3 "  type="submit">Submit</button>
+        <button className="text-xl text-white bg- bg-pink-600 p-2 m-3 " type="submit">Submit</button>
       </form>
 
-      <table border="1">
+      <table border="1" >
         <thead>
           <tr>
             <th>Name</th>
             <th>Age</th>
+            <th>employee</th>
+            <th>unemployee</th>
+
           </tr>
         </thead>
 
@@ -42,6 +45,8 @@ const Table = () => {
             <tr key={index}>
               <td>{item.name}</td>
               <td>{item.age}</td>
+              <td>{item.employee}</td>
+              <td>{item.unemployee}</td>
             </tr>
           ))}
         </tbody>
